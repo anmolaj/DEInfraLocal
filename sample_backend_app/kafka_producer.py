4d4774d467fdd44712f3635187651953fb163eb1
Anmol Jain <anmol.aj@gmail.com>
2024-02-09 18:50:13 +0530

    Format python files using ruff

--- sample_backend_app/kafka_producer.py
+++ sample_backend_app/kafka_producer.py
@@ -6,35 +6,34 @@ import json
 import time
 from kafka import KafkaProducer
 import logging
+
 logging.basicConfig(level=logging.INFO)
+
 
 class StreamKafka:
     def __init__(self, api_url: str) -> None:
         self.api_url = api_url
-        
+
         self.set_producer()
         logging.info("Defined Producer and set URl")
-    
+
     def set_producer(self):
-        self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-    
+        self.producer = KafkaProducer(bootstrap_servers=["localhost:9092"])
+
     def start(self):
         while True:
             resp = requests.get(self.api_url)
             data = resp.json()
 
-            #Adding stream published timestamp which can be used later to track latency
-            data['kafka_created_at'] = time.time_ns()
+            # Adding stream published timestamp which can be used later to track latency
+            data["kafka_created_at"] = time.time_ns()
 
             logging.info(f"Response : {data}")
-            self.producer.send("crypto", json.dumps(data).encode('utf-8'))
+            self.producer.send("crypto", json.dumps(data).encode("utf-8"))
             time.sleep(2)
 
 
-
-
-#def get_response(url: str = "https://api.coindesk.com/v1/bpi/currentprice.json")
-    
+# def get_response(url: str = "https://api.coindesk.com/v1/bpi/currentprice.json")
 
 
 if __name__ == "__main__":
@@ -42,5 +41,5 @@ if __name__ == "__main__":
     """
     Find list of some free apis at: https://apipheny.io/free-api/#apis-without-key
     """
-    stream = StreamKafka(api_url='https://api.coindesk.com/v1/bpi/currentprice.json')
+    stream = StreamKafka(api_url="https://api.coindesk.com/v1/bpi/currentprice.json")
     stream.start()
